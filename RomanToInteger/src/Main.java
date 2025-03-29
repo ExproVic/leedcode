746aeb7a36e8f52695741e091f0763dd93df1675
ExproVic <viktor19970429@gmail.com>
2025-03-29 19:06:47 +0100

    RomanToInteger

---
+++ RomanToInteger/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Put Roman number: ");
+        String roman = scanner.nextLine().toUpperCase();
+        scanner.close();
+
+        RomanToInteger converter = new RomanToInteger();
+        int result = converter.romanToInt(roman);
+
+        System.out.println("Number: " + result);
+    }
+}
+
+class RomanToInteger {
+    public int romanToInt(String s) {
+        int total = 0;
+        int prevValue = 0;
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = romanMap.get(s.charAt(i));
+            if (currentValue < prevValue) {
+                total -= currentValue;
+            } else {
+                total += currentValue;
+            }
+            prevValue = currentValue;
+        }
+
+        return total;
+    }
+}
